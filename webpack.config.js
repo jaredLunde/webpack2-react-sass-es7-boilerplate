2f86d76b5d708ab0fa07999c718c950955229555
Jared Lunde <jared.lunde@gmail.com>
2016-12-09 11:12:57 +0800

    Removed OccurrenceOrderPlugin
    The OccurrenceOrderPlugin has no effect in webpack version 2, since
    this is taken care of automatically

--- webpack.config.js
+++ webpack.config.js
@@ -93,7 +93,6 @@ module.exports = {
         comments: false
       }
     }),
-    new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.LoaderOptionsPlugin({
       minimize: true,
       debug: false
